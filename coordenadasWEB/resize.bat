22bbcaeb9eb8216c8ec6f870f147a0f0fb9dfa1c
Emilio San Miguel <lanchas9@yahoo.com.mx>
2021-02-14 00:02:51 -0600

    ya con fotos reales
    buscador para 56 máximo
    quitado error en animacion
    con PNG de todo el centro al 100% de alpha

--- coordenadasWEB/resize.bat
+++ coordenadasWEB/resize.bat
@@ -1,7 +1,1 @@
-echo off
-SET index=0
-FOR %%A in (*.jp*) DO (
-magick convert -set colorspace sRGB %%A -adaptive-resize 30 %%A
-SET /a index=index+1
-echo %index%
-)
+FOR %%A in (*.jpg) DO (magick %%A -resize 30 %%A)
